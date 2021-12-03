6841ac0cd150bdb60167a0712cdab7735547f757
Deepanshi Gaur <dgaur1@binghamton.edu>
2021-12-03 14:45:38 -0500

    final commit

--- README.md
+++ README.md
@@ -7,21 +7,21 @@
 Replace anything surrounded by the `< >` symbols._
 
 ## SUMMARY:
- < Provide a short description of your program's functionality. No more than a couple sentences. >
+ creates the class LSystem that separates string generation and visualization and Stores L-System rules in a dictionary instance variable of the L-System class, and reads in the rules from the file
 
 ### Final Project Repo Link:
-
+https://github.com/bucs110a0fall21/final-project-amigos-de-frijoles.git
 
 ## GRACE DAYS
-Grace days used for this assignment: < # >
+Grace days used for this assignment: 0
 
-Grace days remaining: < # >/5
+Grace days remaining: 5/5
 
 ## KNOWN BUGS AND INCOMPLETE PARTS:
- < What parts of the project you were not able to complete >
+ None
 
 ## REFERENCES:
- < List any outside resources used >
+ None
 
 ## MISCELLANEOUS COMMENTS:
- < Anything you would like the grader to know >
+ None
--- lsystem.py
+++ lsystem.py
@@ -1,11 +1,70 @@
+import turtle
 class LSystem:
-    #You may add additional methods and break methods up.
-    #Below is the interface that I am expecting in the driver.
-    def __init__(self,filename):
-        #complete this method
+	#You may add additional methods and break methods up.
+	#Below is the interface that I am expecting in the driver.
+	def __init__(self,filename):
+		'''
+		creates the setup for the LSystem
+		args: self, filename
+		return: none
+		'''
+		#complete this method
+		self.rules = {}
+		self.file = open(filename, "r")
+		self.angle = int(self.file.readline())
+		self.iters = int(self.file.readline())
+		self.dist = int(self.file.readline())
+		self.axiom = self.file.readline()
+		
+		for i in self.file:
+			self.split = i.split(" : ")
+			thestr = self.split[1].replace("\n","")
+			self.rules[self.split[0]] = thestr
+		print(self.rules) 
 
-    def createLSystem(self):
-        #complete this method
+	def createLSystem(self):
+		'''
+		creates the LSystem and sets the rules
+		args: self
+		return : none
+		'''
+		#complete this method
+		self.results = self.axiom
+		print(self.axiom)
+		for k in range(int(self.iters)):
+			new_string = ''
+			for j in self.results:
+				try:		
+					new_string += self.rules[j]
+				except KeyError:
+					new_string += j
+			self.results = new_string
 
-    def drawLSystem(self, snap):
-        #complete this method
+		                                      
+	def drawLSystem(self, snap):
+		'''
+		draws the LSystem by following the rules
+		args: self, snap(turtle)
+		return: none 
+		'''
+		#complete this method
+		new_data = []
+		for cmd in self.results:
+			if(cmd == 'F'):
+				snap.forward(self.dist)
+			elif(cmd == '+'):
+				snap.right(self.angle)
+			elif(cmd == '-'):
+				snap.left(self.angle)
+			elif(cmd == '['):
+				snap_state = vars(snap)
+				new_data.append(snap_state)
+			elif(cmd == ']'):
+				if new_data:
+					t_state = new_data.pop()
+					snap.__dict__ = t_state
+
+		
+
+
+		
